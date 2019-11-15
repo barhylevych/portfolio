d13d280e37b784c37871d44f8efc5d2510f55ef4
o.barhylevych <o.barhylevych@iqusion.com>
2019-11-15 12:40:11 +0200

    Updates

---
+++ precache-manifest.caea7ebe26a043c893691a0622a5375d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101fba988c268f49a5a44c24f8135ea0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9490f6c8e7d69c632160",
+    "url": "/portfolio/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "16479721abdafdf23bc8",
+    "url": "/portfolio/static/js/2.55e1b789.chunk.js"
+  },
+  {
+    "revision": "9490f6c8e7d69c632160",
+    "url": "/portfolio/static/js/main.04695b5d.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
