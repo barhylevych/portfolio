9b7cab36a0935548a1ce4f6da7dea31ddce094ea
Oleksandr <obarhyl@gmail.com>
2019-11-16 16:11:42 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1cd8c754fe831940b26d1c128d227400.js"
+  "/portfolio/precache-manifest.ee94a6c8a221a6a4e1818ef8bc379730.js"
 );
 
 self.addEventListener('message', (event) => {
