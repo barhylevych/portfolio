b8cb2766536c734efd3268657dd0a87fec0bedfb
Oleksandr <obarhyl@gmail.com>
2019-11-17 23:11:24 +0200

    Updates

---
+++ precache-manifest.b4a76803962c8dc2823a3a3d5e173984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f28225b928595b2761351d450b98a0d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f54b5cb9f66451ff0922",
+    "url": "/portfolio/static/css/2.d9a8a5a7.chunk.css"
+  },
+  {
+    "revision": "e72c181b730a5a7a44cb",
+    "url": "/portfolio/static/css/main.d394d9f9.chunk.css"
+  },
+  {
+    "revision": "f54b5cb9f66451ff0922",
+    "url": "/portfolio/static/js/2.beae9bff.chunk.js"
+  },
+  {
+    "revision": "e72c181b730a5a7a44cb",
+    "url": "/portfolio/static/js/main.c764ad14.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  }
+]);
